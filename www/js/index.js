418179122f2d5ce9bba300c4fb756569c439b88f
Metalmate <sprenkel_5@msn.com>
2015-03-16 20:57:02 +0100

    Project update

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,120 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+Storage.prototype.setObject = function (key, value)     { this.setItem(key, JSON.stringify(value)); }
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+Storage.prototype.getObject = function (key) {
+    var value   = this.getItem(key);
+    return value && JSON.parse(value);
+}
 
-        console.log('Received Event: ' + id);
+$("#saveForm").on("submit", function()
+{
+    var name        = $("#name").val();
+    var nameAtr1    = $("#name1").val();
+    var valueAtr1   = $("#value1").val();
+    var nameAtr2    = $("#name2").val();
+    var valueAtr2   = $("#value2").val();
+
+    var dataObject  = {
+        name    : name,
+        name1   : nameAtr1,
+        value1  : valueAtr1,
+        name2   : nameAtr2,
+        value2  : valueAtr2 
+    };
+
+    window.localStorage.setObject(name, dataObject);
+
+    $(this).find("input").each(function(){ $(this).val(''); });
+
+    window.location.reload();
+
+    return false;
+});
+
+loadLocalStorage = function ()
+{   
+    var dataObject;
+    var numOfItems  = 0;
+    $("#custom-list").html('');
+    for (var i = 0; i < window.localStorage.length; i++){
+        try
+        {
+            dataObject  = window.localStorage.getObject(window.localStorage.key(i));
+
+            if (!dataObject || !dataObject.name) { continue; }
+
+            $("#custom-list").append('<li><a href="#' + dataObject.name + '" data-transition="slide">' + dataObject.name + '</a> <a href=\'#\' onclick=\'window.localStorage.setObject("' + dataObject.name + '", null); loadLocalStorage(); return false;\'>Verwijder</a></li>');
+
+            $("#home").after(
+                '<div data-role="page" id="' + dataObject.name + '">' +
+                    '<div data-role="header">' +
+                    '<img onclick="$.mobile.changePage(\'#watched\', {transition: \'slide\',reverse: true});" style="float: left; padding:6px; height: 30px; width: 30px;" src="nacho.png">' +
+                    '<h1>Nacho Time!</h1>' +
+                '</div>' +
+                '<div data-role="main" class="ui-content">' +
+                    '<p>Gegevens over: <b>' + dataObject.name + '</b></p>' +
+                    '<table>' +
+                        '<tr>' +
+                            '<td>' + dataObject.name1 + '</td>' +
+                            '<td>' + dataObject.value1 + '</td>' +
+                        '</tr>' +
+                        '<tr>' +
+                            '<td>' + dataObject.name2 + '</td>' +
+                            '<td>' + dataObject.value2 + '</td>' +
+                        '</tr>' +
+                    '</table>' +
+                    '<p><a href="#watched" data-transition="slide" data-direction="reverse">Terug naar lijst met gekeken films, series &amp; tekenfilms</a></p>' +
+                '</div>' +
+                '<div data-role="footer"  data-position="fixed">' +
+                        '<h1>' + dataObject.name + '</h1>' +
+                    '</div>' +
+                '</div>'
+            );
+            numOfItems++;
+        } catch (e)         {}
     }
-};
+    if (numOfItems === 0)   { $("#custom-list").html('<p>Er zijn nog geen gekeken films, series of tekenfilms.</p>'); }
+}
+
+loadLocalStorage();
+
+$(document).on("pagebeforecreate", "#scroll", function(e, ui)
+{
+  var items     = '';
+  for (var i = 1; i < 20; i++)      { items += "<li>" + i + "</li>"; }
+
+  $("#list").append(items);
+});
+
+
+checkScroll     = function ()
+{
+  var activePage    = $.mobile.pageContainer.pagecontainer("getActivePage"),
+    screenHeight    = $.mobile.getScreenHeight(),
+    contentHeight   = $(".ui-content", activePage).outerHeight(),
+    header          = $(".ui-header", activePage).outerHeight() - 1,
+    scrolled        = $(window).scrollTop(),
+    footer          = $(".ui-footer", activePage).outerHeight() - 1,
+    scrollEnd       = contentHeight - screenHeight + header + footer;
+
+  if (activePage[0].id == "scroll" && scrolled >= scrollEnd) { addMore(activePage); }
+}
+
+addMore     = function (page)
+{
+  $(document).off("scrollstop");
+  $.mobile.loading("show");
+
+  setTimeout(function() 
+  {
+    var items   = '',
+      last      = $("li", page).length,
+      cont      = last + 10;
+    for (var i = last; i < cont; i++)   { items += "<li>" + i + "</li>"; }
+
+    $("#list", page).append(items).listview("refresh");
+    $.mobile.loading("hide");
+    $(document).on("scrollstop", checkScroll);
+  }, 500);
+}
+
+$(document).on("scrollstop", checkScroll);
