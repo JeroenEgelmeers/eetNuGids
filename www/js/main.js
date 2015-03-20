fea90a475eaf10176ff716881cf4060b544aee06
Jeroen Egelmeers <mail@jeroenegelmeers.nl>
2015-03-20 16:55:52 +0100

    Eerste opzet van de app (Style van huidge app van eet.nu overgenomen).

---
+++ www/js/main.js
@@ -0,0 +1,65 @@
+var idOfPreviousPage = null;
+
+$(document).ready(function(e)
+{
+	$(window).on("swipeleft", function(e)
+	{
+		if ($.mobile.activePage.attr("id") !== "index" && $.mobile.activePage.attr("id") !== "menu")
+		{
+			$.mobile.changePage("#" + $.mobile.activePage.next().attr("id"), {transition: 'slide'});
+		}
+	});
+
+	/* We should not be able to slide to the right side. - TESTING */
+	$(window).on("swiperight", function(e)
+	{
+		if ($.mobile.activePage.attr("id") !== "menu")
+		{
+ 			$.mobile.changePage("#" + $.mobile.activePage.prev().attr("id"), {transition: 'slide', reverse: true});
+ 		}
+    });
+
+    $(window).on("swipeup", function(e)
+	{
+		if ($.mobile.activePage.attr("id") === "menu")
+		{
+				$.mobile.changePage("#" + idOfPreviousPage, {transition: 'slideup'});
+				idOfPreviousPage = null;
+ 		}
+    });
+
+    $(window).on("swipedown", function(e)
+	{
+		if ($.mobile.activePage.attr("id") !== "menu")
+		{
+ 			idOfPreviousPage = $.mobile.activePage.attr("id");
+	 	 	$.mobile.changePage("#menu", {transition: 'slidedown'});
+ 		}
+    });
+
+    $("#header").toolbar();
+
+    $("#menuBtn").on("tap",function(){
+
+    	if ($.mobile.activePage.attr("id") !== "menu")
+    	{
+    		idOfPreviousPage = $.mobile.activePage.attr("id");
+	 	 	$.mobile.changePage("#menu", {transition: 'slidedown'});
+		}
+		else
+		{
+				$.mobile.changePage("#" + idOfPreviousPage, {transition: 'slideup'});
+				idOfPreviousPage = null;
+		}
+	});
+
+	$("#indexBtn").on("tap", function (e)
+	{
+		$.mobile.changePage('#pickRestaurant', {transition: 'slide'});
+	});
+
+	// Modules
+	 $('.button').on('click',function(){
+       window.location.href = "mailto:support@eetnu.zendesk.com?body=Uw%20vraag%20of%20opmerking."; 
+    });
+});
